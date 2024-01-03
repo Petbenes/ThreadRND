528b93494628439e180c39602e2e0cbd1a5dd74b
Petbenes <129893069+Petbenes@users.noreply.github.com>
2024-01-03 15:56:55 +0100

    Add files via upload

---
+++ ThreadRND/ThreadRND.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int pintRNDNumber;
+
+DWORD WINAPI RNDGenerator (LPVOID lpParam)
+{
+    for (;;)
+    {
+        pintRNDNumber++;
+        if (pintRNDNumber > 6) pintRNDNumber = 1; 
+    }
+    
+    return 0;
+}
+
+DWORD WINAPI RNDReader(LPVOID lpParam)
+{
+    for (int i=0;i<20;i++)
+    {
+        printf("%i,", pintRNDNumber);
+        Sleep(200);
+    }
+
+    return 0;
+}
+
+int main()
+{
+    DWORD ldwThread1Id, ldwThread2Id;
+    HANDLE lhThread2Id;
+
+    // int proměnné
+    pintRNDNumber = 1;
+
+    // spuštění RND threadu
+    CreateThread(NULL, 0, RNDGenerator, NULL, 0, &ldwThread1Id);
+
+    // spuštění čtecího threadu
+    lhThread2Id = CreateThread(NULL, 0, RNDReader, NULL, 0, &ldwThread2Id);
+
+    WaitForSingleObject(lhThread2Id, 20000);
+
+    std::cout << "Hello World!\n";
+}
